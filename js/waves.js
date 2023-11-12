8f59bbbe28c078ab8fb96f3a04f2e7f29e843f4c
gazes12 <khozhainovdmitro@gmail.com>
2023-11-12 12:16:54 +0200

    .

---
+++ js/waves.js
@@ -0,0 +1,7 @@
+// /* waves anim disable for edge BEGIN */
+// if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent)) {
+// 	console.log(1111);
+// 	document.write('<style>.waves--anim svg{animation: none;}</style>')
+// }
+// /* waves anim disable for edge END */
+
